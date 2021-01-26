44b3798dfb0b492196c0fb6b0b6514695d0ad89c
jackisbackpdx <jackson.kelley.g@gmail.com>
2021-01-25 21:15:05 -0800

    fixed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
-const proxy = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 const express = require('express');
 const app = express();
 
-app.use('/api', createProxyMiddleware({ target: 'https://us-central1-socialape-45fc1.cloudfunctions.net'}))
+app.use('/api', createProxyMiddleware({ target: 'https://us-central1-socialape-45fc1.cloudfunctions.net', changeOrigin: true }))
 
 app.listen(3000)
